a607ab1ffd87c7695b23d5a20fef5e14556b3c1c
Virginiiamz <virginia.munoz-castro@iesruizgijon.com>
2024-12-03 18:52:08 +0100

    Modificado checkbox de mostrarTareas.php

--- mostrarTareas.php
+++ mostrarTareas.php
@@ -90,7 +90,25 @@ if (isset($_GET['tareas']) && isset($_GET['listaSeleccionada']) && isset($_GET['
                     ?>
                         <div class="pantallaTareas_contenido">
                             <a class="pantallaTareas_contenido--informacion" href="">
-                                <input type="checkbox" name="" id="" <?php echo $tarea['esrealizada'] ? 'checked' : ''; ?>>
+                                <div class="checkbox-wrapper-12">
+                                    <div class="cbx">
+                                        <input <?php echo $tarea['esrealizada'] ? 'checked' : ''; ?> type="checkbox" id="cbx-12" disabled>
+                                        <label for="cbx-12"></label>
+                                        <svg fill="none" viewBox="0 0 15 14" height="12" width="13">
+                                            <path d="M2 8.36364L6.23077 12L13 2"></path>
+                                        </svg>
+                                    </div>
+
+                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+                                        <defs>
+                                            <filter id="goo-12">
+                                                <feGaussianBlur result="blur" stdDeviation="4" in="SourceGraphic"></feGaussianBlur>
+                                                <feColorMatrix result="goo-12" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 22 -7" mode="matrix" in="blur"></feColorMatrix>
+                                                <feBlend in2="goo-12" in="SourceGraphic"></feBlend>
+                                            </filter>
+                                        </defs>
+                                    </svg>
+                                </div>
                                 <p class="fs-5 tareaInformacion_descripcion"><?php echo $tarea['descripcion'] ?></p>
                                 <p class="fs-5 tareaInformacion_fecha"><?php echo $tarea['fecha_venc'] ?></p>
                                 <?php
