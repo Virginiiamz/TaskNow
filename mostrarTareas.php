f76eab5dafc1aeb9742519e62a8aebedc03d6c06
Virginiiamz <virginia.munoz-castro@iesruizgijon.com>
2024-12-08 19:09:52 +0100

    Cambio de orden en mostrarTareas.php

--- mostrarTareas.php
+++ mostrarTareas.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$listas = require_once('get_listas.php');
 $etiquetas = require_once('get_etiquetas.php');
+$listas = require_once('get_listas.php');
 
 if (isset($_GET['tareas']) && isset($_GET['listaSeleccionada'])) {
     // Decodificar el JSON que recibimos de la URL
