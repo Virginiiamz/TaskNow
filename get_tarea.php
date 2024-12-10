c16e0247027f581fc1f5ec5d897ea28ac19cef6d
Virginiiamz <virginia.munoz-castro@iesruizgijon.com>
2024-12-10 13:28:03 +0100

    Muestra las tareas segun el orden de prioridad de la etiqueta y muestra solo las no completadas

--- get_tarea.php
+++ get_tarea.php
@@ -1,29 +1,36 @@
 <?php
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
 require_once("funcionesBD.php");
 $conexion = obtenerConexion();
 
 // No validamos, suponemos que la entrada de datos es correcta
 $idLista = $_GET['idLista'];
 
-// Definir insert
-$sql_tareas = "SELECT * FROM tarea WHERE id_lista = $idLista";
+$idUsuario = $_SESSION['usuario']['id'];
+
+$tareas_noCompletadas = [];
+
+$sql_tareas_noCompletadas = "SELECT tarea.*, etiqueta.orden_prioridad 
+    FROM tarea
+    JOIN etiqueta ON tarea.id_etiqueta = etiqueta.id AND etiqueta.id_usuario = $idUsuario
+    WHERE tarea.id_lista = $idLista AND esRealizada = 0
+    ORDER BY etiqueta.orden_prioridad ASC";
+$resultado_tareas_noCompletadas = mysqli_query($conexion, $sql_tareas_noCompletadas);
+while ($fila = mysqli_fetch_assoc($resultado_tareas_noCompletadas)) {
+    $tareas_noCompletadas[] = $fila;
+}
+
+// $sql_tareas = "SELECT * FROM tarea WHERE id_lista = $idLista AND id_etiqueta = $etiquetas";
 $sql_lista = "SELECT * FROM lista WHERE id = $idLista";
 
 // Ejecutar consulta
-$resultado_tareas = mysqli_query($conexion, $sql_tareas);
 $resultado_lista = mysqli_query($conexion, $sql_lista);
-
-$tareas = [];
 $listas = mysqli_fetch_assoc($resultado_lista);
 
-while ($fila = mysqli_fetch_assoc($resultado_tareas)) {
-    $tareas[] = $fila;
-}
-
-$tareasJSON = urlencode(json_encode($tareas));
+$tareasNoCompletadasJSON = urlencode(json_encode($tareas_noCompletadas));
 $listaJSON = urlencode(json_encode($listas));
 
-header( "refresh:0;url=mostrarTareas.php?tareas=$tareasJSON&listaSeleccionada=$listaJSON");
+header("refresh:0;url=mostrarTareas.php?tareas=$tareasNoCompletadasJSON&listaSeleccionada=$listaJSON");
 exit();
-
-?>
